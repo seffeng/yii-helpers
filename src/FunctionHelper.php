83ea7f9db5862e54d06c304d83d33ab57ab06717
seffeng <seffeng@sina.cn>
2017-12-13 15:02:01 +0800

    update.

--- src/FunctionHelper.php
+++ src/FunctionHelper.php
@@ -5,7 +5,9 @@
  */
 
 namespace seffeng\helpers;
-
+/**
+ * public function
+ */
 class FunctionHelper
 {
     /**
@@ -16,9 +18,9 @@ class FunctionHelper
      * @return boolean
      */
     public static function isForeach($varName){
-        if(is_array($varName) && count($varName) > 0) return TRUE;
-        if(is_object($varName)) return TRUE;
-        return FALSE;
+        if(is_array($varName) && count($varName) > 0) return true;
+        if(is_object($varName)) return true;
+        return false;
     }
 
     /**
@@ -84,18 +86,18 @@ class FunctionHelper
         $return = false;
         switch($type){
             case 'email'        : { $return = preg_match("/^(\w+[-+.]*\w+)*@(\w+([-.]*\w+)*\.\w+([-.]*\w+)*)$/", $string); break; }
-            case 'http'         : { $return = preg_match("/^http:\/\/[a-za-z0-9-]+\.[a-za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"])*$/", $string); break; }
+            case 'http'         : { $return = preg_match("/^http:\/\/[A-Za-z0-9-]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"])*$/", $string); break; }
             case 'qq'           : { $return = preg_match("/^[1-9]\d{4,11}$/", $string); break; }
             case 'post'         : { $return = preg_match("/^[1-9]\d{5}$/", $string); break; }
-            case 'idnum'        : { $return = preg_match("/^\d{15}(\d{2}[a-za-z0-9])?$/", $string); break; }
-            case 'china'        : { $return = preg_match("/^[".chr(0xa1)."-".chr(0xff)."]+$/", $string); break; } //gbk中文
-            case 'english'      : { $return = preg_match("/^[a-za-z]+$/", $string); break; }
+            case 'idnum'        : { $return = preg_match("/^\d{15}(\d{2}[A-Za-z0-9])?$/", $string); break; }
+            case 'china'        : { $return = preg_match("/^[".chr(0xa1)."-".chr(0xff)."]+$/", $string); break; } //GBK中文
+            case 'english'      : { $return = preg_match("/^[A-Za-z]+$/", $string); break; }
             case 'mobile'       : { $return = preg_match("/^((\(\d{3}\))|(\d{3}\-))?((13)|(14)|(15)|(17)|(18)){1}\d{9}$/", $string); break; }
             case 'phone'        : { $return = preg_match("/^((\(\d{3}\))|(\d{3}\-))?(\(0\d{2,3}\)|0\d{2,3}-)?[1-9]\d{6,7}$/", $string); break; }
-            case 'safe'         : { $return = preg_match("/^(([a-z]*|[a-z]*|\d*|[-_\~!@#\$%\^&\*\.\(\)\[\]\{\}<>\?\\\/\'\"]*)|.{0,5})$|\s/", $string) != 0 ? true : false; break; }
+            case 'safe'         : { $return = preg_match("/^(([A-Z]*|[a-z]*|\d*|[-_\~!@#\$%\^&\*\.\(\)\[\]\{\}<>\?\\\/\'\"]*)|.{0,5})$|\s/", $string) != 0 ? true : false; break; }
             case 'age'          : { $return = (preg_match("/^(-{0,1}|\+{0,1})[0-9]+(\.{0,1}[0-9]+)$/", $string) && intval($string) <= 130 && intval($string) >= 12) ? true : false; break; }
-            case 'eng_num'      : { $return = preg_match("/^[a-za-z0-9]+$/", $string); break; }
-            case 'password'     : { $return = (preg_match("/^[a-za-z0-9]+$/", $string) && strlen($string) <= 32 && strlen($string) >= 6) ? true : false; break; }
+            case 'eng_num'      : { $return = preg_match("/^[A-Za-z0-9]+$/", $string); break; }
+            case 'password'     : { $return = (preg_match("/^[A-Za-z0-9]+$/", $string) && strlen($string) <= 32 && strlen($string) >= 6) ? true : false; break; }
             case 'datetime'     : { $return = preg_match('/^[\d]{4}-[\d]{1,2}-[\d]{1,2}\s[\d]{1,2}:[\d]{1,2}:[\d]{1,2}$/', $string); break; }
             case 'datetimes'    : { $return = preg_match('/^[\d]{4}-[\d]{2}-[\d]{2}\s[\d]{2}:[\d]{2}:[\d]{2}$/', $string); break; }
             case 'date'         : { $return = preg_match('/^[\d]{4}-[\d]{1,2}-[\d]{1,2}$/', $string); break; }
@@ -105,12 +107,12 @@ class FunctionHelper
             case 'ip'           : { $return = preg_match("/^\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b$/", $string); break; }
             case 'incchinese'   : { $return = preg_match('/[\x{4e00}-\x{9fa5}]+/u', $string); break; } //是否包含中文
             case 'plusnum'      : { $return = preg_match('/^[1-9]*[1-9][0-9]*$/', $string); break; } //是否是正整数
-            case 'hostrecord'   : { $return = preg_match('/^[a-z_a-z0-9][a-za-z0-9-]+(\.[a-za-z0-9-_]+)*$/', $string); break; } //正确的主机记录,english
-            case 'cnhostrecord' : { $return = preg_match('/^[_a-za-z0-9]*([\x{4e00}-\x{9fa5}]*[-a-za-z0-9\.]*)+[a-za-z0-9_]$/iu', $string); break; } //正确的主机记录,english chinese
-            case 'domain'       : { $return = preg_match('/^[a-za-z0-9][a-za-z0-9-]+(\.[a-za-z0-9-]+){1,3}$/', $string); break; } //是否是域名
-            case 'cndomain'     : { $return = (preg_match('/[\x{4e00}-\x{9fa5}]+/u', $string) && preg_match('/^([-a-za-z0-9\.]*[\x{4e00}-\x{9fa5}]*[-a-za-z0-9\.]*)+\.(中国|公司|网络|cn|com|net)$/iu', $string)) ? true : false; break; }  //是否中文域名
-            case 'mac'          : { $return = preg_match('/^[a-fa-f\d]{2}:[a-fa-f\d]{2}:[a-fa-f\d]{2}:[a-fa-f\d]{2}:[a-fa-f\d]{2}:[a-fa-f\d]{2}$/', $string); break; }
-            case 'ipv6'         : { $return = preg_match('/^\s*((([0-9a-fa-f]{1,4}:){7}([0-9a-fa-f]{1,4}|:))|(([0-9a-fa-f]{1,4}:){6}(:[0-9a-fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9a-fa-f]{1,4}:){5}(((:[0-9a-fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9a-fa-f]{1,4}:){4}(((:[0-9a-fa-f]{1,4}){1,3})|((:[0-9a-fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9a-fa-f]{1,4}:){3}(((:[0-9a-fa-f]{1,4}){1,4})|((:[0-9a-fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9a-fa-f]{1,4}:){2}(((:[0-9a-fa-f]{1,4}){1,5})|((:[0-9a-fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9a-fa-f]{1,4}:){1}(((:[0-9a-fa-f]{1,4}){1,6})|((:[0-9a-fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9a-fa-f]{1,4}){1,7})|((:[0-9a-fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/', $string); break; }
+            case 'hostrecord'   : { $return = preg_match('/^[A-Z_a-z0-9][A-Za-z0-9-]+(\.[A-Za-z0-9-_]+)*$/', $string); break; } //正确的主机记录,english
+            case 'cnhostrecord' : { $return = preg_match('/^[_a-zA-Z0-9]*([\x{4e00}-\x{9fa5}]*[-a-zA-Z0-9\.]*)+[a-zA-Z0-9_]$/iu', $string); break; } //正确的主机记录,english chinese
+            case 'domain'       : { $return = preg_match('/^[A-Za-z0-9][A-Za-z0-9-]+(\.[A-Za-z0-9-]+){1,3}$/', $string); break; } //是否是域名
+            case 'cndomain'     : { $return = (preg_match('/[\x{4e00}-\x{9fa5}]+/u', $string) && preg_match('/^([-a-zA-Z0-9\.]*[\x{4e00}-\x{9fa5}]*[-a-zA-Z0-9\.]*)+\.(中国|公司|网络|CN|COM|NET)$/iu', $string)) ? true : false; break; }  //是否中文域名
+            case 'mac'          : { $return = preg_match('/^[a-fA-F\d]{2}:[a-fA-F\d]{2}:[a-fA-F\d]{2}:[a-fA-F\d]{2}:[a-fA-F\d]{2}:[a-fA-F\d]{2}$/', $string); break; }
+            case 'ipv6'         : { $return = preg_match('/^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/', $string); break; }
         }
         gettype($return) == 'integer' && $return = $return == 0 ? false : true;
         return $return;
